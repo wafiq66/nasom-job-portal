bfec7947c7308dce004e6cb53171497ab7c9edc1
wafiq66 <155524345+Wafiqsw@users.noreply.github.com>
2025-07-05 18:12:02 +0800

    another goat js

--- my_job_ad/urls.py
+++ my_job_ad/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('employer/JobAd', views.view_job_table,name='view_job_table'),
     #create/update job ad
-    path('employer/JobAd/form', views.post_job,name='post_job'),
+    path('employer/JobAd/form', views.reload_job,name='reload_job'),
+    path('employer/JobAd/save', views.post_job,name='post_job'),
 ]
--- my_job_ad/views.py
+++ my_job_ad/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 
 # Create your views here.
 
 def view_job_table(request):
     return render(request,'advertisement_list.html')
 
+def reload_job(request):
+    if request.method == 'POST':
+        return redirect('reload_job')
+    return render(request, 'write.html')
+
 def post_job(request):
     if request.method == 'POST':
-        return redirect('post_job')
-    return render(request, 'write.html')
+        action = request.POST.get('action')
+
+        if action == 'draft':
+            messages.info(request, "Job saved as draft!")
+        elif action == 'publish':
+            messages.success(request, "Job Advertisement is Successfully Posted!")
+
+        return redirect('view_job_table')
+    return render(request, 'advertisement_list.html')
