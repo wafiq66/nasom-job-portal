f359d27099bf5fde0fdff69d03cace5cae69c415
wafiq66 <155524345+Wafiqsw@users.noreply.github.com>
2025-07-26 01:32:20 +0800

    update setting

--- nasom_job_portal/settings.py
+++ nasom_job_portal/settings.py
@@ -110,7 +110,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')  # Global static folder
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 WSGI_APPLICATION = 'nasom_job_portal.wsgi.application'
